86e5b55bd13d253a16a9f10126f6a31d69b45c2a
Anonymous User 1 <anonymous1@localhost.local>
2025-11-26 15:22:09 +0100

    statistics hinzufügen

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,3 +1,9 @@
+#https://www.pythonguis.com/tutorials/create-ui-with-tkinter-grid-layout-manager/
+
+#hälfte des Fensters ist 600 Pixel 
+
+
+
 import tkinter as tk
 gui = tk.Tk()
 gui.title("Avian - Peer-to-peer file transfers")
@@ -29,4 +35,29 @@ statistics.grid(
     columnspan=2,
     sticky="nsew"
 )
+
+statistics.rowconfigure(0)
+statistics.columnconfigure(0, weight=1)
+statistics.columnconfigure(1, weight=1)
+
+
+active_connections_wrapper = tk.Frame(statistics, bg="red")
+active_connections_wrapper.grid(
+    row=0,
+    column=0,
+    sticky="nsew"
+)
+active_connections = tk.Label(active_connections_wrapper, text="Hallo")
+active_connections.pack(anchor="w")
+
+speed_display_wrapper = tk.Frame(statistics, bg="green")
+speed_display_wrapper.grid(
+    row=0,
+    column=1,
+    sticky="nsew"
+)
+
+speed_display = tk.Label(speed_display_wrapper, text="Hallo")
+speed_display.pack(anchor="e")
+
 gui.mainloop()
