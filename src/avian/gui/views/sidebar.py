99edd9cb4f66cf45749ff4f37b4824b658b23e3f
Anonymous User 2 <anonymous2@localhost.local>
2025-12-03 15:20:00 +0100

    Datei einfügung hinzugefügt

--- src/avian/gui/views/sidebar.py
+++ src/avian/gui/views/sidebar.py
@@ -1,7 +1,26 @@
 import tkinter as tk
-
+from tkinter import filedialog as fd
 class Sidebar(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-
+        def select_file(): 
+            filetypes = ("All files","*.*")
+            filename = fd.askopenfilname(
+                title="open a file "
+                initialder= "/"
+                filtypes=filtypes
+            )
+            showinfo(
+                title="selected a file"
+                message=filename 
         
+          )
+            open_button =tk.Button(
+                text="Open a File "
+                command= select_file
+            )
+            open_button(expand=True)
+
+            self.mainloop ()
+
+         
