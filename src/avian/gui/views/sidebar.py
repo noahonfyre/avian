dff00686739a326c778c60ac608e81821e0c0c73
Anonymous User 1 <anonymous1@localhost.local>
2025-12-10 14:31:19 +0100

    Fehler in Sidebar fixen (wip)

--- src/avian/gui/views/sidebar.py
+++ src/avian/gui/views/sidebar.py
@@ -1,19 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
+
 class Sidebar(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        def select_file(): 
-            filetypes = ("All files","*.*")
-            filename = fd.askopenfilname(title="open a file ", initialder= "/", filetypes=filetypes)
-            showinfo(title="selected a file", message=filename)
-            open_button =tk.Button(
-                text="Open a File "
-                command= select_file
-            )
-            open_button(expand=True)
+        open_button = tk.Button(
+            self,
+            text="Open a File ",
+            command=lambda: self.select_file
+        )
+        open_button.pack(expand=True)
 
-            self.mainloop ()
+    def select_file():
+        filetypes = ("All files","*.*")
+        filename = fd.askopenfilename(title="open a file", filetypes=filetypes)
+        showinfo(title="selected a file", message=filename)
 
          
