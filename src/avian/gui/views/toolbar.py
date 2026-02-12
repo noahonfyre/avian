ee5cc5eaf6bdfabcc7429d91de0370c7aecde95c
Anonymous User 2 <anonymous2@localhost.local>
2026-02-12 15:19:39 +0100

    Toolbar

--- src/avian/gui/views/toolbar.py
+++ src/avian/gui/views/toolbar.py
@@ -9,11 +9,27 @@ if TYPE_CHECKING:
 class Toolbar(tk.Frame):
     def __init__(self, master: GUI):
         super().__init__(master)
-        self.columnconfigure(0, weight=1, uniform="x")
-        self.columnconfigure(1, weight=1, uniform="x")
-        self.rowconfigure(0)
+        self.columnconfigure(0)
+        self.rowconfigure(0, weight=1, uniform= "x")
+        self.rowconfigure(1, weight=1, uniform= "x")
 
+        
         self.configure(padx=5, pady=5)
 
-        self.button = ttk.Button(self, text="test", padding=(0, 0))
+        self.button = ttk.Button(self, text="Connection Window")
         self.button.grid(column=0, row=0, sticky="w")
+        self.button["command"]=comand1 
+
+        def comand1():
+            pass
+
+
+
+
+
+        self.button2 = ttk.Button(self, text="Settings")
+        self.button2.grid(column=0, row=1, sticky="e" )
+        self.button2["command"]=comand2
+
+        def comand2():
+            pass
