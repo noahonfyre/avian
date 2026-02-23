d4c599c5f63ba999378c18eaf42a3a422de11399
noahonfyre <noahonfyre@gmail.com>
2026-02-23 17:29:53 +0100

    feat: add message declarations

---
+++ src/avian/models/messages.py
@@ -0,0 +1,36 @@
+from attr import dataclass
+
+
+@dataclass(frozen=True)
+class Message:
+    pass
+
+
+@dataclass(frozen=True)
+class RejectedConnection(Message):
+    address: str
+    port: int
+    peer_version: int
+
+
+@dataclass(frozen=True)
+class NewConnection(Message):
+    address: str
+    port: int
+
+
+@dataclass(frozen=True)
+class TransactionStart(Message):
+    address: str
+    port: int
+    file_count: int
+
+
+@dataclass(frozen=True)
+class TransactionUpdate(Message):
+    address: str
+    port: int
+    filename: str
+    bytes_transferred: int
+    file_size: int
+    elapsed: float
