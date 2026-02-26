018d6b7ff02f7876b2f2c785d4dca16ac45ca114
Anonymous User 2 <anonymous2@localhost.local>
2026-02-26 15:11:17 +0100

    FIx an der Toolbar

--- src/avian/gui/views/toolbar.py
+++ src/avian/gui/views/toolbar.py
@@ -1,18 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
+from avian.gui.windows.settings.window import SettingsWindow
+from src.avian.gui.windows.connection.window import ConnectionWindow
+
 
 class Toolbar(tk.Frame):
     def __init__(self, master: tk.Tk):
         super().__init__(master)
-        self.columnconfigure(0)
-        self.rowconfigure(0, weight=1, uniform="x")
-        self.rowconfigure(1, weight=1, uniform="x")
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)                
+        self.rowconfigure(0)
+        
 
         self.configure(padx=5, pady=5)
 
         self.button = ttk.Button(self, text="Connection Window")
         self.button.grid(column=0, row=0, sticky="w")
+        self.button["command"]=lambda: ConnectionWindow(self)
 
         self.button2 = ttk.Button(self, text="Settings")
-        self.button2.grid(column=0, row=1, sticky="e")
+        self.button2.grid(column=1, row=0, sticky="e")
+        self.button2["command"]=lambda: SettingsWindow(self)
+            
