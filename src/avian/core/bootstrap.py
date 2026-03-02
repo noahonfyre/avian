b16ba909ba04e3b9cf7b8649e2297b9331edc37c
noahonfyre <noahonfyre@gmail.com>
2026-03-02 22:29:01 +0100

    feat: call receiver and sender from backend

--- src/avian/core/bootstrap.py
+++ src/avian/core/bootstrap.py
@@ -1,8 +1,12 @@
 import threading
-from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
 from avian.models import Channel
+from avian.models.constants import SAVE_PATH
 from avian.models.messages import Message
+from avian.services.receiver_service import ReceiverService
+from avian.services.sender_service import SenderService
+from avian.services.service import Service
 
 
 class Bootstrap(threading.Thread):
@@ -12,11 +16,33 @@ class Bootstrap(threading.Thread):
         self.outgoing = outgoing
 
     def run(self):
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            pass
+        # TODO: change hardcoded values to dynamic inputs
+        threading.Thread(
+            target=run_service,
+            args=(ReceiverService(self.outgoing, SAVE_PATH, 23500),),
+            daemon=True,
+        ).start()
+
+        # TODO: change hardcoded values to dynamic inputs
+        threading.Thread(
+            target=run_service,
+            args=(
+                SenderService(
+                    self.outgoing,
+                    [Path("C:\\Users\\Noah\\Downloads\\20260128 121019.gif")],
+                    "127.0.0.1",
+                    23500,
+                ),
+            ),
+            daemon=True,
+        ).start()
 
     def __enter__(self):
         self.start()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.join()
+
+
+def run_service(service: Service) -> None:
+    service.run()
