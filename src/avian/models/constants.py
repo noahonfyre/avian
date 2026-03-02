982c4e704b256b960cc2f172e7508cc063b29585
noahonfyre <noahonfyre@gmail.com>
2026-03-02 22:15:17 +0100

    refactor: update constants and add paths

--- src/avian/models/constants.py
+++ src/avian/models/constants.py
@@ -1,9 +1,12 @@
 import logging
+from pathlib import Path
 
 ID: str = "avian"
 NAME: str = "Avian"
-VERSION: str = "0.1.0"
+VERSION: int = 1
 PROTOCOL_PORT: int = 9250
 CHUNK_SIZE: int = 4096
+AVIAN_PATH: Path = Path.home() / ".avian"
+SAVE_PATH: Path = AVIAN_PATH / "saved"
 
 LOGGER = logging.getLogger(ID)
