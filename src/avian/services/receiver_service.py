b209b1d63765d0d5e5104d5dfe4b8f616837b227
noahonfyre <noahonfyre@gmail.com>
2026-03-02 22:27:40 +0100

    feat: adjust receiver service to match sender

--- src/avian/services/receiver_service.py
+++ src/avian/services/receiver_service.py
@@ -2,94 +2,93 @@ import socket
 import struct
 import time
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Tuple
 
 from avian.models import Channel
 from avian.models.constants import LOGGER, VERSION
-from avian.models.messages import (
-    Message,
-    NewConnection,
-    RejectedConnection,
-    TransactionStart,
-    TransactionUpdate,
-)
+from avian.models.messages import Message, TransactionStart, TransactionUpdate
 from avian.models.network.protocol import ACK, NACK, recv, send
 from avian.services import Service
 from avian.utils.hashing import verify_file_hash
 
 
 class ReceiverService(Service):
-    def __init__(
-        self,
-        outgoing: Channel[Message],
-        save_path: Path,
-        port: int,
-        sock: Optional[socket.socket] = None,
-    ) -> None:
+    def __init__(self, outgoing: Channel[Message], save_path: Path, port: int) -> None:
         self.outgoing: Channel[Message] = outgoing
         self.save_path: Path = save_path
         self.port: int = port
-        self.sock = sock or socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.file_count: int = 0
 
     def run(self) -> None:
-        LOGGER.info("Initializing receiver service...")
-        if not self.save_path.exists():
-            LOGGER.info("Creating download folder...")
-            self.save_path.mkdir(parents=True)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            LOGGER.info("Initializing receiver service...")
+            if not self.save_path.exists():
+                LOGGER.info("Creating download folder...")
+                self.save_path.mkdir(parents=True)
 
-        LOGGER.info(f"Binding to :{self.port}...")
-        self.sock.bind(("0.0.0.0", self.port))
-        self.sock.listen()
-        LOGGER.info(f"Listening on :{self.port}.")
+            LOGGER.info(f"Binding to :{self.port}...")
+            sock.bind(("0.0.0.0", self.port))
+            sock.listen()
+            LOGGER.info(f"Listening on :{self.port}.")
 
-        while True:
-            conn, addr = self.sock.accept()
-            LOGGER.info(f"Incoming connection: {addr[0]}:{addr[1]}")
+            i = 0
 
-            peer_version: int = struct.unpack("!I", recv(conn))[0]
+            while True:
+                # TODO: change break logic
+                if i > 0:
+                    break
+                conn, addr = sock.accept()
+                try:
+                    self.handle_connection(conn, addr)
+                except Exception as e:
+                    LOGGER.warning(f"Failed to handle {addr[0]}:{addr[1]}: {e}")
+                i += 1
 
-            if peer_version != VERSION:
-                LOGGER.warning(
-                    f"Rejecting {addr[0]}:{addr[1]} due to version mismatch."
-                )
+            LOGGER.info("Concluding sender service...")
+
+    def handle_connection(self, conn: socket.socket, addr: Tuple[str, int]) -> None:
+        LOGGER.info(f"Incoming connection: {addr[0]}:{addr[1]}")
+
+        LOGGER.info("Checking peer version...")
+        peer_version: int = struct.unpack("!I", recv(conn))[0]
+
+        if peer_version != VERSION:
+            LOGGER.warning(f"Rejecting {addr[0]}:{addr[1]} due to version mismatch.")
+            send(conn, NACK)
+            return
+        LOGGER.info("Version match! Continuing...")
+
+        send(conn, ACK)
+
+        self.file_count: int = struct.unpack("!I", recv(conn))[0]
+        self.outgoing.send(TransactionStart(addr[0], addr[1], self.file_count))
+
+        while self.file_count > 0:
+            file: Path = self.receive_file(conn, addr)
+            LOGGER.info("Getting file hash...")
+            file_hash: bytes = struct.unpack("!32s", recv(conn))[0]
+            LOGGER.info("Checking file integrity...")
+            verified: bool = verify_file_hash(file, file_hash)
+            if not verified:
+                LOGGER.warning("File integrity check failed.")
                 send(conn, NACK)
-                self.outgoing.send(
-                    RejectedConnection(
-                        addr[0],
-                        addr[1],
-                        f"Rejecting {addr[0]}:{addr[1]} due to version mismatch.",
-                    )
-                )
-                continue
+                return
 
+            LOGGER.info("File integrity check successful.")
             send(conn, ACK)
+            self.file_count -= 1
 
-            self.outgoing.send(NewConnection(address=addr[0], port=addr[1]))
-
-            self.file_count: int = struct.unpack("!I", recv(self.sock))[0]
-            self.outgoing.send(TransactionStart(addr[0], addr[1], self.file_count))
-
-            while self.file_count > 0:
-                file: Path = self.receive_file(conn, addr)
-                verified: bool = self.verify(conn, file)
-
-                if not verified:
-                    LOGGER.warning(f"{file.name} failed integrity verification.")
-                    send(conn, NACK)
-
-                send(conn, ACK)
-                self.file_count -= 1
+        LOGGER.info(f"Connection with {addr[0]}:{addr[1]} concluded.")
 
     def receive_file(self, conn: socket.socket, addr: Tuple[str, int]) -> Path:
         filename: str = recv(conn).decode()
-        file_size: int = struct.unpack("!I", recv(conn))[0]
+        file_size: int = struct.unpack("!Q", recv(conn))[0]
         LOGGER.info(f"Starting transfer of {filename} ({file_size}B)...")
 
-        destination: Path = self.save_path / filename
+        destination: Path = self.save_path / Path(filename).name
         transferred = 0
 
-        with open(destination, "ab+") as file:
+        with open(destination, "wb") as file:
             while transferred < file_size:
                 start = time.perf_counter()
                 chunk: bytes = recv(conn)
@@ -110,8 +109,3 @@ class ReceiverService(Service):
                 )
         LOGGER.info(f"File {filename} saved in {destination}.")
         return destination
-
-    @staticmethod
-    def verify(conn: socket.socket, destination: Path) -> bool:
-        file_hash = struct.unpack("!32s", recv(conn))[0]
-        return verify_file_hash(destination, file_hash)
