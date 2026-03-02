24d73869e980ca52fedc3b269cd10770a07fadb5
noahonfyre <noahonfyre@gmail.com>
2026-03-02 22:28:34 +0100

    fix: change protocol to use buffered `recv_exact` function

--- src/avian/models/network/protocol.py
+++ src/avian/models/network/protocol.py
@@ -21,11 +21,21 @@ def send(conn: socket.socket, payload: bytes) -> None:
     conn.sendall(HEADER.pack(MAGIC, len(payload)) + payload)
 
 
+def recv_exact(conn: socket.socket, size: int) -> bytes:
+    buffer = b""
+    while len(buffer) < size:
+        chunk = conn.recv(size - len(buffer))
+        if not chunk:
+            raise ConnectionError("Frame reception failed.")
+        buffer += chunk
+    return buffer
+
+
 def recv(conn: socket.socket) -> bytes:
-    header: bytes = conn.recv(HEADER_SIZE)
-    magic, payload_length = HEADER.unpack_from(header)
+    header: bytes = recv_exact(conn, HEADER_SIZE)
+    magic, payload_length = HEADER.unpack(header)
 
     if magic != MAGIC:
         raise ValueError(f"Protocol constant mismatch: magic={hex(magic)}")
-    payload: bytes = conn.recv(payload_length)
+    payload: bytes = recv_exact(conn, payload_length)
     return payload
