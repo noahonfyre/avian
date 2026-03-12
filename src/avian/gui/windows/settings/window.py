e681c1e687a47f9f0efcadacdbbee1134aa4a3dc
Anonymous User 2 <anonymous2@localhost.local>
2026-03-12 14:11:24 +0100

    Settings FIx

--- src/avian/gui/windows/settings/window.py
+++ src/avian/gui/windows/settings/window.py
@@ -5,18 +5,18 @@ class SettingsWindow(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
 
-        notebook = ttk.Notebook(parent)
-        notebook.pack(pady=10, expand=True)
+        notebook = ttk.Notebook(self)
+        notebook.grid(pady=10)
 
 
         frame1 = ttk.Frame(notebook, width=400, height=280)
         frame2 = ttk.Frame(notebook, width=400, height=280)
         frame3 = ttk.Frame(notebook, width=400, height=280)
 
-        frame1.pack(fill= "both", expand=True)
-        frame2.pack(fill= "both", expand=True)
-        frame3.pack(fill= "both", expand=True)
-
+        frame1.grid(sticky="nswe")
+        frame2.grid(sticky="nswe")
+        frame3.grid(sticky="nswe")
+        
         notebook.add(frame1, text = "General")
         notebook.add(frame2, text = "Download")
         notebook.add(frame3, text = "Upload")
