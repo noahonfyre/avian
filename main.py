ee9e7edf908fbb28a3049c42a2f520a08235271f
noahonfyre <noahonfyre@gmail.com>
2026-03-14 20:34:12 +0100

    feat: SIGTERM and SIGINT on top of errors and GUI closing will now cause the program to shut down

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import logging
+import signal
+import sys
 import traceback
-from pathlib import Path
 
 from avian.core.bootstrap import Bootstrap
 from avian.gui.gui import GUI
 from avian.models.channel import Channel
 from avian.models.constants import ID, LOGGER, VERSION
 from avian.models.messages import Message
+
+event_channel: Channel[Message] = Channel(Message, -1)
+command_channel: Channel[Message] = Channel(Message, -1)
 
 
 def main() -> None:
@@ -16,25 +20,30 @@ def main() -> None:
         datefmt="%H:%M:%S",
     )
 
-    run_path: Path = Path(__file__).parent
-
     LOGGER.info(f"Initializing {ID} v{VERSION}...")
 
-    event_channel: Channel[Message] = Channel(Message, -1)
-    command_channel: Channel[Message] = Channel(Message, -1)
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
 
     try:
-        gui: GUI = GUI(event_channel, command_channel, run_path)
-        with Bootstrap(command_channel, event_channel, run_path):
+        gui: GUI = GUI(event_channel, command_channel)
+        with Bootstrap(command_channel, event_channel):
             gui.run()
     except Exception as e:
         # TODO: add better error handling
         traceback.print_exception(type(e), e, e.__traceback__)
 
+    shutdown(0, None)
+
+
+def shutdown(signum: int, _) -> None:
+    LOGGER.info("Shutdown signal received...")
     LOGGER.info("Shutting down...")
 
     event_channel.close()
     command_channel.close()
+
+    sys.exit(signum)
 
 
 if __name__ == "__main__":
