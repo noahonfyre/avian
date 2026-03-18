abd3597f9e330b7b4d38f219db5a04f448a7884e
noahonfyre <noahonfyre@gmail.com>
2026-03-18 21:35:31 +0100

    refactor: remove upper limit for chunk size

--- src/avian/models/config/config.py
+++ src/avian/models/config/config.py
@@ -6,7 +6,7 @@ class Config:
     SPEC = ConfigSpec(AVIAN_PATH / "config.json")
 
     PROTOCOL_PORT = SPEC.define(ConfigField("protocol_port", 9250, lambda i: 0 < i < 65_536))
-    CHUNK_SIZE = SPEC.define(ConfigField("chunk_size", 4096, lambda i: 0 < i < 16_384))
+    CHUNK_SIZE = SPEC.define(ConfigField("chunk_size", 4096, lambda i: 0 < i))
     SAVE_PATH = SPEC.define(ConfigField("save_path", str(AVIAN_PATH / "saved")))
 
     @classmethod
