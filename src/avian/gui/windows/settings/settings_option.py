4fa90110d333317a4e3621429723dbfdd5bad88d
noahonfyre <noahonfyre@gmail.com>
2026-03-18 22:38:52 +0100

    feat: add settings option helper class

---
+++ src/avian/gui/windows/settings/settings_option.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class SettingsOption(tk.Frame):
+    def __init__(self, master, text: str):
+        super().__init__(master)
+
+        self.config(padx=5)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0)
+
+        self.heading = ttk.Label(self, text=text)
+        self.heading.grid(column=0, row=0, sticky="we")
+
+        self.wrapper = tk.Frame(self)
+        self.wrapper.grid(column=0, row=1, sticky="nswe")
+
+        self.wrapper.columnconfigure(0, weight=1)
