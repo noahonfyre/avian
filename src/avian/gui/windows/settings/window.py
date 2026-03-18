0564d6c76677a707c3d4f62363403c48ef25ecad
noahonfyre <noahonfyre@gmail.com>
2026-03-18 22:39:18 +0100

    feat: unify about section and general settings in settings window

--- src/avian/gui/windows/settings/window.py
+++ src/avian/gui/windows/settings/window.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 
-from avian.gui.windows.settings.about import AboutTab
-from avian.gui.windows.settings.general import GeneralTab
+from avian.gui.windows.settings.general import GeneralSection
+from avian.models.constants import NAME, VERSION
+from avian.models.resources import get_resource
 
 
 class SettingsWindow(tk.Toplevel):
@@ -10,16 +11,41 @@ class SettingsWindow(tk.Toplevel):
         super().__init__(parent)
 
         self.title("Settings")
-        self.geometry("400x450")
-        self.minsize(400, 450)
+        self.geometry("550x300")
+        self.minsize(550, 300)
         self.transient(parent)
         self.grab_set()
 
-        self.columnconfigure(0, weight=1)
+        self.icon = tk.PhotoImage(data=get_resource("icon.png").read_bytes())
+
+        self.columnconfigure(0)
+        self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=1)
+        self.rowconfigure(1)
 
-        self.notebook = ttk.Notebook(self)
-        self.notebook.grid(row=0, column=0, sticky="nsew")
+        self.overview = ttk.Frame(self, padding=10)
+        self.overview.grid(column=0, row=0, sticky="nsew")
 
-        self.notebook.add(GeneralTab(self.notebook), text="General")
-        self.notebook.add(AboutTab(self.notebook), text="About")
+        self.app_icon = tk.PhotoImage(data=get_resource("icon.png").read_bytes())
+
+        self.icon_label = ttk.Label(self.overview, image=self.app_icon)
+        self.icon_label.grid()
+
+        self.version_label = ttk.Label(self.overview, text=f"{NAME} v{VERSION}")
+        self.version_label.grid(pady=5)
+
+        self.tagline_label = ttk.Label(
+            self.overview,
+            text="Here is a placeholder of the tagline of the application",
+            wraplength=128,
+        )
+        self.tagline_label.grid()
+
+        self.copyright_label = ttk.Label(self, text="© 2025-2026 | MIT License")
+        self.copyright_label.grid(column=0, row=1, padx=5)
+
+        self.general = GeneralSection(self)
+        self.general.grid(column=1, row=0, sticky="nsew", padx=5)
+
+        self.save_button = ttk.Button(self, text="Save", command=self.general.apply_config)
+        self.save_button.grid(column=1, row=1, sticky="se", padx=5, pady=5)
